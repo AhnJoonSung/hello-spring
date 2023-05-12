158c467af870ff74de0aba2617ba247ada276ac6
AhnJoonSung <tjdtna01@naver.com>
2023-05-12 15:56:36 +0900

    .

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,26 +1,14 @@
 package hello.hellospring;
-
-import hello.hellospring.domain.Member;
-import hello.hellospring.repository.JpaMemberRepository;
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.persistence.EntityManager;
-
+import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
-
-//    private EntityManager em;
-//
-//    @Autowired
-//    public SpringConfig(EntityManager em) {
-//        this.em = em;
-//    }
-
     private final MemberRepository memberRepository;
 
     @Autowired
@@ -33,9 +21,10 @@ public class SpringConfig {
         return new MemberService(memberRepository);
     }
 
-    @Bean
-    public MemberRepository memberRepository() {
-
-        return new MemoryMemberRepository();
-    }
+//    @Bean
+//    public MemberRepository memberRepository() {
+//// return new MemoryMemberRepository();
+//        //return new JpaMemberRepository(em);
+//
+//    }
 }
